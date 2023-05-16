d3aabc7d06b5f6ea557d6d62d79468d8e5a653a3
Error Studios <113478929+MattMurillo@users.noreply.github.com>
2023-05-16 16:36:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+
+function preload()
+{
+song = loadSound("");
+}
+
+function setup()
+{
+    canvas = createCanvas(500, 600);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+    console.log(results);
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("left Wrist X = " +leftWristX+ "left Wrist Y = " +leftWristY);
+
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("right Wrist X = " +rightWristX+ "right Wrist Y = " +rightWristY);
+    }
+}
+
+function modelLoaded()
+{
+    console.log("Model is Initialized");
+}
